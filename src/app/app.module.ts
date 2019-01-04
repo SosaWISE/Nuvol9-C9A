4f93f33d827ae2f5f8d2213025079285b3ba83cf
Andres Sosa <andres@wisearchitects.com>
2019-01-03 23:22:38 -0700

    Save changes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +13,9 @@ import { AUTH_CONTAINER_DECLARATIONS } from './authForm';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/authForm/authForm.component.ts
+++ src/app/authForm/authForm.component.ts
@@ -1,5 +1,6 @@
 //** LIBRARIES */
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
 
 @Component({
     selector: 'auth',
@@ -9,18 +10,37 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 export class AuthFormComponent implements OnInit {
     //** PROPERTIES AND METHODS */
     @Output('onAuthenticate') onAuthenticate: EventEmitter<boolean> = new EventEmitter<boolean>();
+    public frmMain: FormGroup;
+
+    //** CONSTRUCTOR AND DI */
+    constructor(private formBuilder: FormBuilder) {
+
+    }
 
     ngOnInit(): void {
         //** Init */
         let self = this;
 
+        //** Init Form */
+        self.initForm();
+    }
+
+    private initForm(): void {
+        //** Init */
+        let self = this;
+
+        self.frmMain = self.formBuilder.group({
+            userName: new FormControl(null, [Validators.required]),
+            password: new FormControl(null, [Validators.required]),
+            rememberMe: new FormControl(null)
+        });
     }
 
     public login(): void {
         //** Init */
         let self = this;
 
-        alert("Yes you are tryng to login.");
+        alert(`Yes you are tryng to login with u:${self.frmMain.controls["userName"].value} | P: ${self.frmMain.controls["password"].value} | R: ${self.frmMain.controls["rememberMe"].value}`);
         self.onAuthenticate.emit(true);
     }
 
