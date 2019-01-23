4e937ccb36ebf54faedfcb1e2bc73534ee97d113
Andres Sosa <andres@wisearchitects.com>
2019-01-23 00:07:19 -0700

    Added a grid result.

---
+++ src/app/models/root.search.models.ts
@@ -0,0 +1,7 @@
+export interface ISearchResultItem {
+    claimNumber: string;
+    claimStatus: number;
+    fullName: string;
+    address: string;
+    phone: string;
+}
--- src/app/root/root.search.component.ts
+++ src/app/root/root.search.component.ts
@@ -1,11 +1,61 @@
 //** LIBRARIES */
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+//** MODELS */
+import { ISearchResultItem } from '../models/root.search.models';
 
 @Component({
     selector: 'root-search-component',
     templateUrl: './root.search.component.html',
     styleUrls: ['./root.search.component.scss']
 })
-export class RootSearchComponent {
+export class RootSearchComponent implements OnInit {
+    /** PORPERTIES */
+    public searchResult: ISearchResultItem[] = [];
 
+    /** CONSTRUCTOR */
+    constructor() {}
+
+    //**#region START Base Methods */
+    ngOnInit(): void {
+        //** Init */
+        let self = this;
+
+        self.searchResult = [{
+            claimNumber: '001234-AC01',
+            claimStatus: 1,
+            fullName: 'Sammy E. Jones',
+            address: '794 E 3650 N; Provo UT 84604',
+            phone: '(801) 807-2345'
+        },{
+            claimNumber: '001235-AC01',
+            claimStatus: 1,
+            fullName: 'Jackson A. Aurthor',
+            address: '1240 E Canyon View Dr.; Logan UT 84604',
+            phone: '(801) 789-0987'
+        },{
+            claimNumber: '001236-AC01',
+            claimStatus: 1,
+            fullName: 'Manuel Aragon Ortega',
+            address: '890 N Mohavie Ln; Murrey UT 84003',
+            phone: '(801) 234-0987'
+        },{
+            claimNumber: '001237-AC01',
+            claimStatus: 1,
+            fullName: 'Sammy E. Jones',
+            address: '794 E 3650 N; Provo UT 84604',
+            phone: '(801) 807-2345'
+        },{
+            claimNumber: '001238-AC01',
+            claimStatus: 1,
+            fullName: 'Sammy E. Jones',
+            address: '794 E 3650 N; Provo UT 84604',
+            phone: '(801) 807-2345'
+        }];
+    }
+    //**#endregion END   Base Methods */
+
+    public submitMe(): void {
+        alert("JES is true.  I'm Nacho...");
+    }
 }
