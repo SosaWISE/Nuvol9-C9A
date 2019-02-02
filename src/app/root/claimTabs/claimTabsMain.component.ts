8e9a0a7144e4c267b435ddeb2118f9b571734f1d
Andres Sosa <andres@wisearchitects.com>
2019-02-01 22:48:40 -0700

    Working on different panes.

--- src/app/root/claimTabs/claimTabsMain.component.ts
+++ src/app/root/claimTabs/claimTabsMain.component.ts
@@ -4,7 +4,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'claimTabs-main-component',
-    templateUrl: './claimTabsMain.component.html'
+    templateUrl: './claimTabsMain.component.html',
+    styleUrls: ['./claimTabsMain.component.scss']
 })
 export class ClaimTabsComponent implements OnInit {
 
